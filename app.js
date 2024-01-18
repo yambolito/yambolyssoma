9aaa647d5a72d8f84ba3a25ca4866be4a8e565e7
yamboly <seguridad.sst@yamboly.com>
2024-01-18 17:39:35 -0500

    newss

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-require("./aludar.js");
+
+
+
+
+class Car{
+    cars(name,year){
+        this.name =name;
+        this.year = year;
+
+    }
+}
+
+
+const car1 = new Car("toyota",2015);
+
+document.getElementById("demo").innerHTML = car1.name+ "" +car1.year
